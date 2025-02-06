1205cc3ba409bfeea8de1e41734c5450597a7f63
Ivan Kobzarev <ivanloid@gmail.com>
2025-02-06 10:50:04 +0300

    Поправил поиск элементов на странице

--- test/duckduckgo_tests/test_duckduckgo_search.py
+++ test/duckduckgo_tests/test_duckduckgo_search.py
@@ -4,5 +4,5 @@ from selene import browser, have
 #todo_unsuccessful duckduckgo search
 def test_unsuccesfull_duckduckgo_search():
     query = 'ajssdfghhdfsfhhdshfhsdfhdfdhfshsdfhfds'
-    browser.element('[role="combobox"]').type(query).press_enter()
+    browser.element('[role=combobox]').type(query).press_enter()
     browser.element('.w7syQmNN6Yjvw6guGJuQ').should(have.text(f"По запросу {query} результаты не найдены."))
--- test/niffler_tests/test_niffler.py
+++ test/niffler_tests/test_niffler.py
@@ -3,60 +3,60 @@ from selene import browser, have, be
 
 #Ввести корректные креды и попасть на главную
 def test_succesfull_login():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345')
-    browser.element('button[type="submit"]').click()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345')
+    browser.element('[type=submit]').click()
+    browser.element('#spendings').should(have.text('History of Spendings'))
 
 #Авторизоваться с помощью клавиатуры
 def test_succesfull_login_with_enter():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345').press_enter()
+    browser.element('#spendings').should(have.text('History of Spendings'))
 
 #Авторизация с некорректными кредами
 def test_unsuccesfull_login_with_wrong_credentials():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('123456').press_enter()
-    browser.element('[class="form__error"]').should(have.text('Неверные учетные данные пользователя'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('123456').press_enter()
+    browser.element('.form__error').should(have.text('Неверные учетные данные пользователя'))
 
 #Проверка имени пользователя в лк (через all)
 def test_succesfull_login_check_username_all():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-    browser.element('[data-testid="PersonIcon"]').click()
-    browser.all('[role="menuitem"]')[0].should(have.text('Profile')).click()
-    browser.element('[id="username"]').should(have.value('stas'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345').press_enter()
+    browser.element('#spendings').should(have.text('History of Spendings'))
+    browser.element('[data-testid=PersonIcon]').click()
+    browser.all('[role=menuitem]')[0].should(have.text('Profile')).click()
+    browser.element('#username').should(have.value('stas'))
 
 #Проверка имени пользователя в лк (через element)
 def test_succesfull_login_check_username_element():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
-    browser.element('[data-testid="PersonIcon"]').click()
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345').press_enter()
+    browser.element('#spendings').should(have.text('History of Spendings'))
+    browser.element('[data-testid=PersonIcon]').click()
     browser.element('.link.nav-link').should(have.text('Profile')).click()
-    browser.element('[id="username"]').should(have.value('stas'))
+    browser.element('#username').should(have.value('stas'))
 
     
 #Проверка выхода из авторизованной зоны (через all)
 def test_succesfull_logout_all():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345').press_enter()
+    browser.element('#spendings').should(have.text('History of Spendings'))
     # todo check logout
-    browser.element('[data-testid="PersonIcon"]').click()
-    browser.all('[role="menuitem"]')[3].should(have.text('Sign out')).click()
+    browser.element('[data-testid=PersonIcon]').click()
+    browser.all('[role=menuitem]')[3].should(have.text('Sign out')).click()
     browser.element('//button[text()="Log out"]').click()
     browser.element('.header').should(be.visible).should(have.text('Log in'))
 
 #Проверка выхода из авторизованной зоны (через element)
 def test_succesfull_logout_element():
-    browser.element('input[name="username"]').type('stas')
-    browser.element('input[name="password"]').type('12345').press_enter()
-    browser.element('[id="spendings"]').should(have.text('History of Spendings'))
+    browser.element('[name=username]').type('stas')
+    browser.element('[name=password]').type('12345').press_enter()
+    browser.element('#spendings').should(have.text('History of Spendings'))
     # todo check logout
-    browser.element('[data-testid="PersonIcon"]').click()
+    browser.element('[data-testid=PersonIcon]').click()
     browser.element('ul.MuiList-root.MuiList-padding.MuiMenu-list li:last-child').should(have.text('Sign out')).click()
     browser.element('//button[text()="Log out"]').click()
     browser.element('.header').should(be.visible).should(have.text('Log in'))
